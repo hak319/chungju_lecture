a841528b22da3633519fc474a87bc97968c83d8f
SMART24\User <fds44fs@nate.com>
2025-05-19 10:58:38 +0900

    restart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chungju_lecture/todo_local_page.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do 프로젝트',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      routes: {
+        '/': (context) => const HomePage(),
+        '/todo-local': (context) => const TodoLocalPage()
+      },
+    );
+  }
+}
